96c6ee25bee28d3946f335d993fa2723baeee242
Vadim Babajanyan <babadzhanyan@corp.mail.ru>
2022-09-11 18:18:59 +0300

    github

--- WaterCounter/SSDP.ino
+++ WaterCounter/SSDP.ino
@@ -8,11 +8,11 @@ void SSDP_init(void) {
 	SSDP.setSchemaURL("description.xml");
 	SSDP.setHTTPPort(80);
 	SSDP.setName(SSDP_Name);
-	SSDP.setSerialNumber("000000000001");
+	SSDP.setSerialNumber("000000000002");
 	SSDP.setURL("/");
 	SSDP.setModelName("WaterCounter");
-	SSDP.setModelNumber("000000000001");
-	SSDP.setModelURL("https://bitbucket.org/smarthometeam_AKEB/watercounter");
+	SSDP.setModelNumber("000000000002");
+	SSDP.setModelURL("https://github.com/AKEB/watercounter");
 	SSDP.setManufacturer("Vadim Babajanyan");
 	SSDP.setManufacturerURL("http://akeb.ru");
 	SSDP.begin();
